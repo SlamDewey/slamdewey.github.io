4c9bcc83bdf0be699ce5954c969769144f9393a1
SlamDewey <mr.jaredmassa@gmail.com>
2019-02-20 15:41:33 -0500

    added a premium canvas backdrop animatino with JS

--- backdrop.js
+++ backdrop.js
@@ -7,19 +7,51 @@ var canvas = document.querySelector('canvas');
 set_bounds();
 
 var c = canvas.getContext('2d');
+var circles = [];
+var mouse = {
+    x: undefined,
+    y: undefined,
+    vx: undefined,
+    vy: undefined
+};
+var rad_max = 50;
+var rad_min = 10;
+var range = 50;
+var mousedown = false;
+var colors = [
+    'darkred',
+    'darkblue',
+    'darkgreen',
+    'darkorange',
+    'gray'
+];
 
-function Circle(x, y, radius, vx, vy) {
+window.addEventListener('mousemove', function(event) {
+    mouse.vx = (event.x - mouse.x) / 4;
+    mouse.vy = (event.y - mouse.y) / 4;
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+window.addEventListener('mousedown', function(event) {
+    mousedown = true;
+});
+window.addEventListener('mouseup', function(event) {
+    mousedown = false;
+});
+
+function Circle(x, y, radius, vx, vy, color) {
     this.x = x;
     this.y = y; 
     this.radius = radius;
     this.vx = vx;
     this.vy = vy;
+    this.color = color;
 
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'lightblue';
-        c.stroke();
+        c.fillStyle = colors[color];
+        c.fill();
     }
     this.tick = function() {
         if (this.x + this.radius > innerWidth) this.vx = -Math.abs(this.vx);
@@ -28,18 +60,30 @@ function Circle(x, y, radius, vx, vy) {
         else if (this.y - this.radius < 0) this.vy = Math.abs(this.vy);
         this.x += this.vx;
         this.y += this.vy;
+        if (Math.abs(mouse.x - this.x) < range && Math.abs(mouse.y - this.y) < range) {
+            if (this.radius < rad_max) this.radius += 1;
+            if (mousedown) {
+                this.vx = mouse.vx;
+                this.vy = mouse.vy;
+            }
+        }
+        else if (this.radius > rad_min){
+            this.radius -= 1;
+        }
         this.draw();
     }
 }
 
-var circles = [];
-for (var i = 0; i < 150; i++) {
+
+for (var i = 0; i < 500; i++) {
+    var radius = rad_min;
     circles.push(new Circle(
-        (Math.random() * innerWidth),
-        (Math.random() * innerHeight),
-        30,
-        (Math.random() - 0.5) * 10,
-        (Math.random() - 0.5) * 10
+        (Math.random() * (innerWidth - radius * 2) + radius),
+        (Math.random() * (innerHeight - radius * 2) + radius),
+        radius,
+        (Math.random() - 0.5) * 3,
+        (Math.random() - 0.5) * 3,
+        Math.floor((Math.random() * colors.length))
     ));
 }
 
