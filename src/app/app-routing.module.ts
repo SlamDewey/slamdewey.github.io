5fcd71217d56b7217915263330928ee3e617c70a
Jared Massa <mr.jaredmassa@gmail.om>
2022-09-08 23:37:48 -0400

    added gallery component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
   }
 ];
 
