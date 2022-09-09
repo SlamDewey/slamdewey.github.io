2468d97924ca15d287dd5dc0f726ac081af5efc9
Jared Massa <mr.jaredmassa@gmail.om>
2022-09-09 12:45:37 -0400

    experimentation

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,3 +1,4 @@
+import { makeBindingParser } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SiteHeaderComponent } from './site-header/site-header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     SiteHeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [
     SiteHeaderComponent
