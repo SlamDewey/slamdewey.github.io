f9eae45069c664a3f863080befadc04a00d12290
Jared Massa <mr.jaredmassa@gmail.om>
2022-11-12 23:16:25 -0500

    reformatted projects page and added button to hide text and enjoy fractal

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Backdrop } from '../shared/backdrop/backdrop';
 import { NewtownsFractalWebGLBackground } from '../shared/backdrop/NewtownsFractalWebGLBackground';
@@ -10,13 +10,12 @@ import { NewtownsFractalWebGLBackground } from '../shared/backdrop/NewtownsFract
 })
 export class ProjectsComponent {
 
+  @ViewChild("container") container: HTMLDivElement;
+
   public bgAnimation: Backdrop = new NewtownsFractalWebGLBackground();
+  public shouldHide: boolean = false;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Hobbies & Projects');
   }
-
-  setShaderMode(i: number) {
-  }
-
 }
