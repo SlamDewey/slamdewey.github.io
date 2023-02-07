bace8aaf8219ceea50f11620f1fcfdfc010d85c4
Jared Massa <mr.jaredmassa@gmail.om>
2023-02-06 20:06:09 -0500

    updated the projects page and respective newtons backdrop to be more accessible

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Backdrop } from '../shared/backdrop/backdrop';
+import { BackdropComponent } from '../shared/backdrop/backdrop.component';
 import { NewtownsFractalWebGLBackground } from '../shared/backdrop/NewtownsFractalWebGLBackground';
 
 @Component({
@@ -12,10 +13,21 @@ export class ProjectsComponent {
 
   @ViewChild("container") container: HTMLDivElement;
 
-  public bgAnimation: Backdrop = new NewtownsFractalWebGLBackground();
+  public bgAnimation: NewtownsFractalWebGLBackground = new NewtownsFractalWebGLBackground();
   public shouldHide: boolean = false;
+  public isWebGlEnabled: boolean = BackdropComponent.isWebGlEnabled;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Hobbies & Projects');
+  }
+
+  public incrementIterationCount(): void {
+    if (this.bgAnimation.iterations < this.bgAnimation.MAX_ITERATIONS)
+      this.bgAnimation.iterations++;
+  }
+
+  public decrementIterationCount(): void {
+    if (this.bgAnimation.iterations > this.bgAnimation.MIN_ITERATIONS)
+      this.bgAnimation.iterations--;
   }
 }
--- src/app/shared/backdrop/NewtownsFractalWebGLBackground.ts
+++ src/app/shared/backdrop/NewtownsFractalWebGLBackground.ts
@@ -14,15 +14,19 @@ export class NewtownsFractalWebGLBackground extends WebGLBackdrop {
     gl_Position = vec4(coordinates.xy, 0.0, 1.0);
   }
   `;
+
+  public MAX_ITERATIONS: number = 50;
+  public MIN_ITERATIONS: number = 1;
+
   readonly FragmentShader: string = `
   precision mediump float;
   
-  const int MAX_ITERATIONS = 20;
-  const float PI = 3.14159;
+  const int MAX_ITERATIONS = ${this.MAX_ITERATIONS};
   const float brightness = 0.8;
   
   uniform vec2 screenSize;
   uniform float time;
+  uniform int iter;
 
   vec2 complex_mul(vec2 a, vec2 b) {
     return vec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
@@ -48,33 +52,16 @@ export class NewtownsFractalWebGLBackground extends WebGLBackdrop {
     return complex_mul(vec2(3.0, 0.0), complex_pow(x, vec2(2.0, 0.0)));
   }
   
-  vec2 g(vec2 x) {
-    return complex_pow(x, vec2(6.0, 0.0)) + complex_pow(x, vec2(3.0, 0.0)) - vec2(1.0, 0.0);
-  }
-  vec2 gp(vec2 x) {
-    return complex_mul(vec2(6.0, 0.0), complex_pow(x, vec2(5.0, 0.0))) + complex_mul(vec2(3.0, 0.0), complex_pow(x, vec2(2.0, 0.0)));
-  }
-  
-  vec2 newtonsMethod(int function, vec2 x0, vec2 iterationOffset) {
+  vec2 newtonsMethod(vec2 x0, vec2 iterationOffset) {
     vec2 xn = x0;
-    if (function == 1) {
-      for (int n = 0; n < MAX_ITERATIONS; n++) {
-        xn = xn - complex_div(f(xn), fp(xn)) + iterationOffset;
-        //xn = xn - complex_div(f(xn), fp(xn));
+    for (int n = 0; n < MAX_ITERATIONS; n++) {
+      xn = xn - complex_div(f(xn), fp(xn)) + iterationOffset;
+      if (iter < n) {
+        break; 
       }
-    }
-    else if (function == 2) {
-      for (int n = 0; n < MAX_ITERATIONS; n++) {
-        xn = xn - complex_div(g(xn), gp(xn)) + iterationOffset;
-        //xn = xn - complex_div(g(xn), gp(xn));
-      }
-    }
-    else {
-      return vec2(0.0);
     }
     return xn;
   }
-  
   void fColor(vec2 iterated_location) {
     vec2 uv = gl_FragCoord.xy / screenSize.xy;
     
@@ -95,92 +82,23 @@ export class NewtownsFractalWebGLBackground extends WebGLBackdrop {
       gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
     }
   }
-  void gColor(vec2 iterated_location) {
-    vec2 uv = gl_FragCoord.xy / screenSize.xy;
-    
-    float r = 0.85179964207;
-    float r_sqrt3over2 = 0.73768012896;
-    float half_r = 0.42589982103;
-    
-    float t = -1.17398499671;
-    float t_sqrt3over2 = -1.01670083081;
-    float half_t = 0.58699249835;
-    
-    float dist1 = complex_sqdist(iterated_location, vec2(r, 0.0));
-    float dist2 = complex_sqdist(iterated_location, vec2(-half_r, r_sqrt3over2));
-    float dist3 = complex_sqdist(iterated_location, vec2(-half_r, -r_sqrt3over2));
-    float dist4 = complex_sqdist(iterated_location, vec2(t, 0.0));
-    float dist5 = complex_sqdist(iterated_location, vec2(-half_t, t_sqrt3over2));
-    float dist6 = complex_sqdist(iterated_location, vec2(-half_t, -t_sqrt3over2));
-    
-    
-    float test = dist1;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-      //gl_FragColor = vec4(((sin(time) + 1.0) / 2.0) * brightness, 0.0, ((cos(time) + 1.0) / 2.0) * brightness, 1.0);
-      return;
-    }
-    test = dist2;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-      //gl_FragColor = vec4(((sin(time) + 1.0) / 2.0) * brightness, ((cos(time) + 1.0) / 2.0) * brightness, 0.0, 1.0);
-      return;
-    }
-    test = dist3;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-      //gl_FragColor = vec4(0.0, ((sin(time) + 1.0) / 2.0) * brightness, ((cos(time) + 1.0) / 2.0) * brightness, 1.0);
-      return;
-    }
-    test = dist4;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-      //gl_FragColor = vec4(((cos(time) + 1.0) / 2.0) * brightness, 0.0, ((sin(time) + 1.0) / 2.0) * brightness, 1.0);
-      return;
-    }
-    test = dist5;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-      //gl_FragColor = vec4(((cos(time) + 1.0) / 2.0) * brightness, ((sin(time) + 1.0) / 2.0) * brightness, 0.0, 1.0);
-      return;
-    }
-    test = dist6;
-    if (test <= dist1 && test <= dist2 && test <= dist3 && test <= dist4 && test <= dist5 && test <= dist6) {
-      gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-      //gl_FragColor = vec4(0.0, ((cos(time) + 1.0) / 2.0) * brightness, ((sin(time) + 1.0) / 2.0) * brightness, 1.0);
-      return;
-    }
-    
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-  }
-  
-  void colorFrag(int function, vec2 iterated_location) {
-    if (function == 1) {
-      fColor(iterated_location);
-    }
-    else if (function == 2) {
-      gColor(iterated_location);
-    }
-    else {
-      gl_FragColor = vec4(gl_FragCoord.xy / screenSize.xy, (sin(time) + 1.0) / 2.0, 1.0);
-    }
-  }
   
   void main() {
     vec2 relCoordinate = (gl_FragCoord.xy - (screenSize.xy / 2.0)) / (screenSize.x);
     
     //float scalar = 10.0 * ((sin(time / 5.0) + 1.0) / 2.0) + 3.0;
-    //float scalar = 10.0 * ((-cos(time / 15.0) + 1.0) / 2.0);
-    float scalar = 2.4;
+    float scalar = 10.0 * ((-cos(time / 15.0) + 1.0) / 2.0);
+    //float scalar = 2.4;
     
-    int function = 1;
-    
-    vec2 iterated_location = newtonsMethod(function, relCoordinate * scalar, -vec2(0.4 * sin(time / 2.0), 0.4 * cos(time / 2.0)));
-    //vec2 iterated_location = newtonsMethod(function, relCoordinate * scalar, vec2(0.0, 0.0));
-    //vec2 iterated_location = newtonsMethod(function, relCoordinate * scalar, vec2(sin(time) * 0.2, 0.0));
-    colorFrag(function, iterated_location);
+    vec2 iterated_location = newtonsMethod(relCoordinate * scalar, -vec2(0.4 * sin(time / 2.0), 0.4 * cos(time / 2.0)));
+    //vec2 iterated_location = newtonsMethod(relCoordinate * scalar, vec2(0.0, 0.0));
+    //vec2 iterated_location = newtonsMethod(relCoordinate * scalar, vec2(sin(time) * 0.2, 0.0));
+    fColor(iterated_location);
   }
   `;
+
+  public iterations: number = this.MIN_ITERATIONS;
+
   protected override getVertexShader(): string {
     return this.VertexShader;
   }
@@ -195,6 +113,7 @@ export class NewtownsFractalWebGLBackground extends WebGLBackdrop {
 
     gl.uniform2f(gl.getUniformLocation(shaderProgram, "screenSize"), this.width, this.height);
     gl.uniform1f(gl.getUniformLocation(shaderProgram, "time"), this.time);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "iter"), this.iterations);
   }
 
   protected update(deltaTime: number): void {
@@ -204,5 +123,6 @@ export class NewtownsFractalWebGLBackground extends WebGLBackdrop {
   protected override prepareDrawVariables(gl: WebGLRenderingContext, deltaTime: number): void {
     this.time += deltaTime;
     gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "time"), this.time);
+    gl.uniform1i(gl.getUniformLocation(this.shaderProgram, "iter"), this.iterations);
   }
 }
