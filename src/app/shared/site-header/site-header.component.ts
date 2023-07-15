eb7f51534fb291d4671b5ef89dab464e5b315045
Jared Massa <mr.jaredmassa@gmail.om>
2023-07-15 15:41:03 -0400

    updated site header to show which page is active

--- src/app/shared/site-header/site-header.component.ts
+++ src/app/shared/site-header/site-header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'x-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.scss']
 })
-export class SiteHeaderComponent { }
+export class SiteHeaderComponent implements OnInit {
+
+  public activeRoute: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd)
+        this.onRouteChange()
+    });
+  }
+
+  private onRouteChange() {
+    const splitRoute: string[] = this.router.url.split('/');
+    this.activeRoute = splitRoute.length < 1 ? '' : splitRoute[1];
+  }
+}
