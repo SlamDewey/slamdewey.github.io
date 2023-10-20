d2dcf0524699382cf668a0792908d19f79e5daa1
Jared Massa <mr.jaredmassa@gmail.om>
2023-10-19 23:23:24 -0400

    home page updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,31 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { Backdrop } from '../shared/backdrop/backdrop';
-import { ShaderTestAnimatedBackground } from '../shared/backdrop/ShaderTestAnimatedBackground';
+import { Component, ViewEncapsulation, OnInit } from "@angular/core";
+import { Backdrop } from "../shared/backdrop/backdrop";
+import { UVColorCycleBackground } from "../shared/backdrop/UVColorCycleBackground";
 
-const HELLOS: string[] = ['hello', 'hello', 'hi', 'howdy', 'hey'];
+const HELLOS: string[] = ["hello", "hi", "howdy", "hey"];
 
 const GREETING_MESSAGES: string[] = [
-  'thanks for visiting',
-  'enjoy your stay',
+  "thanks for visiting",
+  "enjoy your stay",
   "how'd you end up here?",
-  'welcome to my site',
-  'take off the coat and stay a while',
-  'thanks for checking in',
+  "welcome to my site",
+  "take off the coat and stay a while",
+  "thanks for checking in",
   "congratulations, you've made it",
-  'welcome to the other side',
-  'do you come here often?',
-  'there is nothing suspicious here',
+  "welcome to the other side",
+  "do you come here often?",
+  "there is nothing suspicious here",
+  "now that you're here, you can never leave",
 ];
 
 @Component({
-  selector: 'x-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "x-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  public bgAnimation: Backdrop = new ShaderTestAnimatedBackground();
+  public bgAnimation: Backdrop = new UVColorCycleBackground();
   public hello: string;
   public greeting: string;
 
