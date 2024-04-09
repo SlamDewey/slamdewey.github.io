41d116f1f65d46961b3924acd2da4301031699bc
Jared Massa <mr.jaredmassa@gmail.om>
2024-04-09 19:44:13 -0400

    just removing the newtons fractal project link since it is old

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,8 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
-import {
-  NewtownsFractalWebGLBackground,
-  PositionalChoice,
-} from "./pages/newtonsfractal/NewtownsFractalWebGLBackground";
 import { ReactiveWebGLBackground } from "./pages/fragmentwriter/ReactiveWebGLBackground";
 import { MOUSE_POSITION_NEWTONS_FRACTAL_SHADER } from "./pages/fragmentwriter/shader-programs";
 import { PerlinNoiseBackdrop } from "../shared/backdrop/PerlinNoiseBackdrop";
@@ -16,17 +12,11 @@ export class ProjectsComponent implements OnInit {
   @ViewChild("test", { static: false }) testCanvas: ElementRef;
 
   public bgAnimation = new PerlinNoiseBackdrop();
-  public newtonsFractalAnimation: NewtownsFractalWebGLBackground;
   public fragmentWriterTileBackdrop: ReactiveWebGLBackground;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.newtonsFractalAnimation = new NewtownsFractalWebGLBackground();
-    this.newtonsFractalAnimation.iterations = 5;
-    this.newtonsFractalAnimation.positionalChoice =
-      PositionalChoice.CIRCULAR_MOTION;
-
     this.fragmentWriterTileBackdrop = new ReactiveWebGLBackground();
     this.fragmentWriterTileBackdrop.shaderProgramData =
       MOUSE_POSITION_NEWTONS_FRACTAL_SHADER;
