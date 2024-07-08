5bab1e3bebbe59adc236f679d366474173307ab2
Jared Massa <mr.jaredmassa@gmail.om>
2024-07-08 19:54:14 -0400

    adding basic unit tasking stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule, Routes } from '@angular/router';
-import { GalleryComponent } from './gallery/gallery.component';
-import { HomeComponent } from './home/home.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { NewtonsfractalComponent } from './projects/pages/newtonsfractal/newtonsfractal.component';
-import { FragmentwriterComponent } from './projects/pages/fragmentwriter/fragmentwriter.component';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule, Routes } from "@angular/router";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { NewtonsfractalComponent } from "./projects/pages/newtonsfractal/newtonsfractal.component";
+import { FragmentwriterComponent } from "./projects/pages/fragmentwriter/fragmentwriter.component";
 
 export type FaviconStorage = {
   faviconPath: string;
@@ -12,44 +12,44 @@ export type FaviconStorage = {
 
 export const routes: (Route & FaviconStorage)[] = [
   {
-    path: '',
+    path: "",
     component: HomeComponent,
-    title: 'Jared Massa | Software Engineer',
-    faviconPath: 'home-favicon.ico',
+    title: "Jared Massa | Software Engineer",
+    faviconPath: "home-favicon.ico",
   },
   {
-    path: 'gallery',
+    path: "gallery",
     component: GalleryComponent,
-    title: 'Gallery',
-    faviconPath: 'photo-favicon.ico',
+    title: "Gallery",
+    faviconPath: "photo-favicon.ico",
   },
   {
-    path: 'projects',
+    path: "projects",
     component: ProjectsComponent,
-    title: 'Projects',
-    faviconPath: 'project-favicon.ico',
+    title: "Projects",
+    faviconPath: "project-favicon.ico",
   },
   {
-    path: 'projects',
+    path: "projects",
     children: [
       {
-        path: 'newtonsfractal',
+        path: "newtonsfractal",
         title: "Newton's Fractal, animated",
         component: NewtonsfractalComponent,
       },
       {
-        path: 'fragmentwriter',
-        title: 'Web based GLSL Fragment Shader Editor',
+        path: "fragmentwriter",
+        title: "Web based GLSL Fragment Shader Editor",
         component: FragmentwriterComponent,
       },
     ],
-    faviconPath: 'projects-favicon.ico',
+    faviconPath: "projects-favicon.ico",
   },
   {
-    path: '**',
-    pathMatch: 'full',
+    path: "**",
+    pathMatch: "full",
     component: HomeComponent,
-    faviconPath: '',
+    faviconPath: "",
   },
 ];
 
---
+++ src/app/projects/pages/unittasking/unittasking.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "x-unittasking",
+  templateUrl: "./unittasking.component.html",
+  styleUrls: ["./unittasking.component.scss"],
+})
+export class UnitTaskingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
