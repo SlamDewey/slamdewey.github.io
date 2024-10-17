fa77efbbc660d96ee7da337d4a7d45cbac399824
Jared Massa <mr.jaredmassa@gmail.om>
2024-10-17 00:47:46 -0400

    docs (deploy): small tweaks to deploy script

--- deploy.bat
+++ deploy.bat
@@ -3,16 +3,13 @@
 :: This .bat file serves as a script to create, commit, and push new build artifact.
 
 @echo off
-echo Building project...
 call npm run build:prod
 
 :: we need to create this 404.html for ghpages to work with angular router
 echo Duplicating index.html into 404.html
 type "%CD%\docs\index.html" >> "%CD%\docs\404.html"
 
-echo Staging Commits...
 call git add docs/*
 call git commit -m "build (auto-rebuild): deploy.bat is creating this new build artifact"
-echo Pushing...
 call git push
-echo Manual Deploy Succesful
+echo Deploy Complete
